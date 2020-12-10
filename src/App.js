6416eab1a4d72492d2e59f64bc6111321404e9ef
Carlos <arq.camejia@gmail.com>
2020-12-09 22:13:50 -0400

    Separated static zone from the slider area

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import DescriptionArea from './Home/DescriptionArea';
 import Arrows from './Home/Arrows';
 import AboutProducts from './Home/AboutProducts';
 
-function App() {
+const App = () => {
 	return (
 		<div className='box-border w-screen h-screen grid grid-cols-10 grid-rows-3'>
-			<div className='bg-chair-img bg-no-repeat bg-cover bg-center col-start-1 col-end-7 row-start-1 row-end-3'>
+			{/* static area */}
+			<div className='col-start-1 col-end-7 row-start-1 row-end-3 z-10'>
 				<NavBar />
 			</div>
-			<div className='col-start-7 col-end-11 row-start-1 row-end-3'>
-				<DescriptionArea />
-			</div>
-			<div className='col-start-7 col-end-8 row-start-1 row-end-3 self-end'>
+			<div className='col-start-7 col-end-8 row-start-1 row-end-3 self-end z-10'>
 				<Arrows />
 			</div>
-			<div className='col-start-1 col-end-4 row-start-3 row-end-4 bg-dark-chairs'></div>
+
+			{/* Slider area */}
+			<div className='col-start-1 col-end-11 row-start-1 row-end-3'>
+				<div className='w-full h-full grid grid-cols-10 grid-rows-1'>
+					<div className='col-start-1 col-end-7 bg-chair-img bg-no-repeat bg-cover bg-center'></div>
+					<div className='col-start-7 col-end-11'>
+						<DescriptionArea />
+					</div>
+				</div>
+			</div>
+
+			<div className='col-start-1 col-end-4 row-start-3 row-end-4 bg-dark-chairs bg-no-repeat bg-cover bg-center'></div>
 			<div className='col-start-4 col-end-8 row-start-3 row-end-4'>
 				<AboutProducts />
 			</div>
-			<div className='col-start-8 col-end-11 row-start-3 row-end-4 bg-white-chair'></div>
+			<div className='col-start-8 col-end-11 row-start-3 row-end-4 bg-white-chair bg-no-repeat bg-cover bg-center'></div>
 		</div>
 	);
-}
+};
 
 export default App;
--- src/Home/Arrows.js
+++ src/Home/Arrows.js
@@ -4,10 +4,10 @@ import rightArrow from '../assets/icon-angle-right.svg';
 const Arrows = () => {
 	return (
 		<div className='w-full'>
-			<button className='bg-black w-3/6 h-20 inline-flex justify-center items-center'>
+			<button className='bg-black hover:bg-gray-900 w-3/6 h-20 inline-flex justify-center items-center'>
 				<img src={leftArrow} alt='' />
 			</button>
-			<button className='bg-black w-3/6 h-20 inline-flex justify-center items-center'>
+			<button className='bg-black hover:bg-gray-900 w-3/6 h-20 inline-flex justify-center items-center'>
 				<img src={rightArrow} alt='' />
 			</button>
 		</div>
